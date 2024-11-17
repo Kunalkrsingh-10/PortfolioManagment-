4c2011a81a9b08fa68be319318836a7aeb942c03
Kunalkrsingh-10 <kunalkuma2115@gmail.com>
2024-11-17 10:34:52 +0530

    complete

--- Base/models.py
+++ Base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name=models.CharField(max_length=50)
+    Email=models.EmailField(max_length=100)
+    Number=models.IntegerField()
+    content=models.TextField(max_length=400) 
+
--- Base/views.py
+++ Base/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import contact
+from django.contrib import messages
 
 # Create your views here.
 
 
-def home(request):
-    return render(request, "home.html")
+#def home(request):
+    #return render(request, "home.html") 
+
+def contact(request):
+    if request.method == "POST":
+        print("post")
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        number=request.POST.get('phone')
+        content=request.POST.get('content')
+        print(name,email, content, number)
+        if len(name)>1 and len(name)<30:
+            pass
+        else: 
+            messages.error(request,"Length of name should be btw 1 and 30 characters")
+            return render(request, 'home.html') 
+        if len(email)>1 and len(email)<30:
+            pass
+        else: 
+            messages.error(request,"Invalide email ,try again ")
+        
+        new_contact = contact(name=name, Email=email, Number=number, content=content)
+        new_contact.save()
+        messages.success(request, "Thank you for connecting me ") 
+    return render(request, "home.html")    
